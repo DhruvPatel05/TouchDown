3fb9d2cc5772f48385f709bcdfd7a74fce8c975f
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-24 22:08:07 -0700

    Main common test cases committed.

---
+++ TouchDown/TouchDownTests/TouchDownTests.swift
@@ -0,0 +1,19 @@
+//
+//  TouchDownTests.swift
+//  TouchDownTests
+//
+//  Created by Dhruv Patel on 24/07/26.
+//
+
+import Testing
+@testable import TouchDown
+
+struct TouchDownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
