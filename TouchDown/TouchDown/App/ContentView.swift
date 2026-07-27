d22ceb65623f1518c6efc307cdcc1d51b3bff795
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-27 11:24:52 -0700

    Contentview Updated as per the touchdown app.

---
+++ TouchDown/TouchDown/App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 24/07/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 12 Pro")
+    }
+}
+
