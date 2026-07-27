e1864b9d7e9022abdc8d32c739f56baedd73b3ae
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-27 11:25:25 -0700

    Footerview added in home screen.

---
+++ TouchDown/TouchDown/View/Home/FooterView.swift
@@ -0,0 +1,22 @@
+//
+//  FooterView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 27/07/26.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text("Created by Dhruv Patel")
+            .padding()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
