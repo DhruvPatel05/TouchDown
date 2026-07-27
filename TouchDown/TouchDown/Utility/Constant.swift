04c8c4340cf41119a9873ecd46c15f824e35a07b
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-27 11:25:12 -0700

    Constant added to use globally.

---
+++ TouchDown/TouchDown/Utility/Constant.swift
@@ -0,0 +1,36 @@
+//
+//  Constant.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 27/07/26.
+//
+
+import SwiftUI
+
+// DATA
+
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+
+// UX
+
+
+// API
+
+
+// IMAGE
+
+
+// FONT
+
+
+// STRING
+
+
+// MISC
+
