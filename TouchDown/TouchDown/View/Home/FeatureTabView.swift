83984da8aa2785494bb2494a6c23a5bad74ea4cd
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-29 13:06:27 -0700

    Feature tab view added.

---
+++ TouchDown/TouchDown/View/Home/FeatureTabView.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureTabView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 29/07/26.
+//
+
+import SwiftUI
+
+struct FeatureTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureTabView()
+}
