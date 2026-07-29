d098e860e47c103406cf62e6cbdc0008b20061d4
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-29 13:06:39 -0700

    Feature item View added.

---
+++ TouchDown/TouchDown/View/Home/FeaturedItemView.swift
@@ -0,0 +1,37 @@
+//
+//  FeaturedItemView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 29/07/26.
+//
+
+import SwiftUI
+
+struct FeaturedItemView: View {
+    // MARK: -  PROPERTIES
+    let player:Player
+    
+    
+    // MARK: -  BODY
+
+    var body: some View {
+        Image(player.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+// MARK: -  PREVIEW
+struct FeaturedItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItemView(player: players[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
+
+
+
+
