f4a7cea867f380cbec9c9cc6c3ae4bb44458eed2
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-29 13:05:45 -0700

    Data model created for Player.

---
+++ TouchDown/TouchDown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 29/07/26.
+//
+
+import Foundation
+
+struct Player:Codable,Identifiable {
+    let id:Int
+    let image:String
+}
