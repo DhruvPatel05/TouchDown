4e9567251815cd8c8a6f6e2c0273a2e400e55a5c
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-29 13:05:18 -0700

    Logoview Added.

---
+++ TouchDown/TouchDown/View/Home/LogoView.swift
@@ -0,0 +1,42 @@
+//
+//  LogoView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 29/07/26.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30,height: 30,alignment: .center)
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }//: HSTACK
+    }
+}
+
+// MARK: -  PREVIEW
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+        
+    }
+}
+
+
+
+
