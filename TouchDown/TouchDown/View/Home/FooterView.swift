2f3d4576fdf98e97749f391ee712da3a0bfc3ec4
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-28 18:28:05 -0700

    Footerview Updated.

--- TouchDown/TouchDown/View/Home/FooterView.swift
+++ TouchDown/TouchDown/View/Home/FooterView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct FooterView: View {
     var body: some View {
-        Text("Created by Dhruv Patel")
-            .padding()
-            .foregroundColor(.secondary)
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, confortable, lightweight and durable football helmets in the market at a affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            Image("logo-linea1")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+            Text("Copyrigtht © Dhruv Patel\nAll right reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        }//: VSTACK
+        .padding()
     }
 }
 
