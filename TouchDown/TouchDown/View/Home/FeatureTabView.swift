0e342b72b205c9de389b67441100912721365850
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-30 10:34:02 -0700

    Feature tabview Updated.

--- TouchDown/TouchDown/View/Home/FeatureTabView.swift
+++ TouchDown/TouchDown/View/Home/FeatureTabView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct FeatureTabView: View {
+    
+    // MARK: -  PREOPERTIES
+
+    // MARK: -  BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ForEach(players) { player in
+                FeaturedItemView(player:player)
+                    .padding(.top,10)
+                    .padding(.horizontal,15)
+            }
+        }//:TAB
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
     }
 }
 
-#Preview {
-    FeatureTabView()
+// MARK: -  PREVIEW
+struct FeaturedTabView_previews: PreviewProvider {
+    static var previews: some View {
+        FeatureTabView()
+            .previewDevice("iPhone 17 Pro")
+            .background(Color.gray)
+    }
 }
