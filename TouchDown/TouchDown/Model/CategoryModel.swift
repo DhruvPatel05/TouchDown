f5490a15f569d8ffe1354b7a50f413b05cb1dbb7
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-31 21:58:59 -0700

    Category Model Added.

---
+++ TouchDown/TouchDown/Model/CategoryModel.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryModel.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 30/07/26.
+//
+
+import Foundation
+
+struct CategoryModel: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+    
+}
