945e4ce9f97c722b53fc818b3c931246f0301c2f
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-04 17:47:52 -0700

    Product Model Added.

--- TouchDown/TouchDown/Model/ProductModel.swift
+++ TouchDown/TouchDown/Model/ProductModel.swift
@@ -13,5 +13,13 @@ struct Product: Codable, Identifiable {
     let price: Double
     let description: String
     let image: String
-    let color:[Color]
+    let color:[Double]
+    
+    var red:Double {return color[0] }
+    var green:Double {return color[1] }
+    var blue:Double {return color[2] }
+    
+    var formattedPrice: String {
+        return "$\(price)"
+    }
 }
