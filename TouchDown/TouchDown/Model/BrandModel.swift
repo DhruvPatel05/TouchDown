d200500eeb0a29c1bf2bf6ac172d72b7b3c660bf
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-06 15:13:56 -0700

    New Model created for brand.

---
+++ TouchDown/TouchDown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 06/08/26.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id:Int
+    let image:String
+}
