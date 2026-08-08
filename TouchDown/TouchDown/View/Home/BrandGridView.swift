8fe158c92b60c968e17e4ee057fc5ce8f549833e
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-07 21:47:15 -0700

    New BrandGridView Added

---
+++ TouchDown/TouchDown/View/Home/BrandGridView.swift
@@ -0,0 +1,18 @@
+//
+//  BrandGridView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 07/08/26.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrandGridView()
+}
