5a705f48c15d8435762c1b8696846bf1c58a7ea9
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-07 21:58:25 -0700

    NEW custom grid view added.

--- TouchDown/TouchDown/View/Home/BrandGridView.swift
+++ TouchDown/TouchDown/View/Home/BrandGridView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct BrandGridView: View {
+    
+    // MARK: -  PROPERTIES
+    
+    
+    
+    // MARK: -  BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-#Preview {
-    BrandGridView()
+// MARK: -  PREVIEW
+
+struct BrandGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+    }
+    
 }
+
+
+
+
