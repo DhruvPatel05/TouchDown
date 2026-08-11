5485288f832ce0465b2297ec643e3397e4e003af
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-10 23:38:01 -0700

    NavigationbarDetailview added.

---
+++ TouchDown/TouchDown/View/Detail/NavigationbarDetailView.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationbarDetailView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 10/08/26.
+//
+
+import SwiftUI
+
+struct NavigationbarDetailView: View {
+    // MARK: -  PROPERTY
+    
+    
+    // MARK: -  BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: -  PREVIEW
+struct NavigationbarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationbarDetailView()
+    }
+}
