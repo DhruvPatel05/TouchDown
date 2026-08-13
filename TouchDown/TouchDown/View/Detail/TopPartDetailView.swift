1c49ac3537bd137cbb50115e1af50e92a264c4e8
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-13 12:45:58 -0700

    ToppartDetailview updated.

--- TouchDown/TouchDown/View/Detail/TopPartDetailView.swift
+++ TouchDown/TouchDown/View/Detail/TopPartDetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TopPartDetailView: View {
     
     // MARK: -  PROPERTY
-    
+    @State private var isAnimating: Bool = false
     // MARK: -  BODY
     var body: some View {
         HStack(alignment: .center,spacing: 6, content: {
@@ -23,10 +23,19 @@ struct TopPartDetailView: View {
                 .fontWeight(.black)
                 .scaleEffect(1.35,anchor:.leading)
             })
+            .offset(y:isAnimating ? -50:-75)
             Spacer()
             //:PHOTO
-            
+            Image(sampleProduct.image)
+                .resizable()
+                .scaledToFit()
+                .offset(y:isAnimating ? 0 : -35)
         })//:HSTACK
+        .onAppear(perform:  {
+            withAnimation(.easeOut(duration:0.75)) {
+                isAnimating.toggle()
+            }
+        })
     }
 }
 
