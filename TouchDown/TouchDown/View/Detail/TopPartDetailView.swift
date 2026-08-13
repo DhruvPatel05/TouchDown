2f56975b321a2f6616ad68e784ccf70659a1e7d2
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-13 12:15:20 -0700

    ToppartDetailview Added.

---
+++ TouchDown/TouchDown/View/Detail/TopPartDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  TopPartDetailView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 13/08/26.
+//
+
+import SwiftUI
+
+struct TopPartDetailView: View {
+    
+    // MARK: -  PROPERTY
+    
+    // MARK: -  BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -  PREVIEW
+struct TopPartDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopPartDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+
+
+
+
