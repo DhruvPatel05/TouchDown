880dc2484b8638fdad6a6496755cfb4c21639ac0
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-12 22:39:03 -0700

    HeaderDetailView Updated.

--- TouchDown/TouchDown/View/Detail/HeaderDetailView.swift
+++ TouchDown/TouchDown/View/Detail/HeaderDetailView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct HeaderDetailView: View {
+    // MARK: -  PROPERTY
+    
+    
+    
+    // MARK: -  BODY
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 6, content: {
+            Text("Protective Gear")
+            Text(sampleProduct.name)
+                .font(.largeTitle)
+                .fontWeight(.black)
+
+        })//: VSTACK
+        .foregroundColor(.white)
     }
 }
 
-#Preview {
-    HeaderDetailView()
+// MARK: -  PREVIEW
+
+struct HeaderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
 }
+
+
+
+
+
+
