dbc585906371c91ec9fd9c8d5450035706d551f8
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-14 22:20:26 -0700

    CustomShape Class added.

---
+++ TouchDown/TouchDown/Utility/CustomShape.swift
@@ -0,0 +1,18 @@
+//
+//  CustomShape.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 14/08/26.
+//
+
+import SwiftUI
+
+struct CustomShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomShape()
+}
