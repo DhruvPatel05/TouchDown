d9b8fdfc4079868a843bf13d993cd491db95c121
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-16 23:22:09 -0700

    RatingSizeDetailView Added.

---
+++ TouchDown/TouchDown/View/Detail/RatingSizeDetailview.swift
@@ -0,0 +1,34 @@
+//
+//  RatingSizeDetailview.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 16/08/26.
+//
+
+import SwiftUI
+
+struct RatingSizeDetailview: View {
+    // MARK: -  PROPERTY
+    
+    
+    // MARK: -  BODY
+    
+
+
+    var body: some View {
+        
+    }
+}
+
+// MARK: -  PREVIEW
+
+
+struct RatingSizeDetailview_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingSizeDetailview()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+
+
