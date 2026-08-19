4ca7f273eb45d84d985bb457f7e2249a90715837
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-18 17:55:21 -0700

    QuantityfavouriteDetailView Added.

---
+++ TouchDown/TouchDown/View/Detail/QuantityFavouriteDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  QuantityFavouriteDetailView.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 18/08/26.
+//
+
+import SwiftUI
+
+struct QuantityFavouriteDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuantityFavouriteDetailView()
+}
