d40bdd31fcd3452027822c28d018bb46fae207ef
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-19 20:29:28 -0700

    Quantity Favourite Detailview UI updated

--- TouchDown/TouchDown/View/Detail/QuantityFavouriteDetailView.swift
+++ TouchDown/TouchDown/View/Detail/QuantityFavouriteDetailView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct QuantityFavouriteDetailView: View {
+    // MARK: -  PROPERTY
+    @State private var counter: Int = 0
+    
+    
+    // MARK: -  BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center,spacing:6, content: {
+            Button(action: {},label: {
+               Image(systemName: "minus.circle")
+            })
+            
+            Text("\(counter)")
+                .fontWeight(.semibold)
+                .frame(minWidth:36)
+                
+        })//: HSTACK
+        .font(.system(.title,design: .rounded))
+        .foregroundColor(.black)
+        .imageScale(.large)
     }
 }
 
-#Preview {
-    QuantityFavouriteDetailView()
+// MARK: -  PREVIEW
+struct QuantityFavouriteDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuantityFavouriteDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
 }
+
