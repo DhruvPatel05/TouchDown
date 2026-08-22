129f6d459c637bf5a8954ee128a3a31adef62ef2
Dhruv Patel <dhruvpatel913@gmail.com>
2026-08-21 22:55:43 -0700

    Shop Object added.

---
+++ TouchDown/TouchDown/Utility/Shop.swift
@@ -0,0 +1,13 @@
+//
+//  Shop.swift
+//  TouchDown
+//
+//  Created by Dhruv Patel on 21/08/26.
+//
+
+import Foundation
+
+class Shop {
+    var showingProduct: Bool = false
+    var selectedProduct: Product? = nil
+}
